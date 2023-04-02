94c4cf81228e40bc2cbf93c4a1aea42178cecb32
Moritz Schmidt <moritz.schmidt@hs-augsburg.de>
2023-04-02 14:01:05 +0200

    Add yank to system clipboard support

--- lua/setup/remaps.lua
+++ lua/setup/remaps.lua
@@ -4,4 +4,10 @@ vim.g.mapleader = ','
 vim.keymap.set('n', '<leader>dd', ':Lexplore %:p:h<CR>')
 vim.keymap.set('n', '<leader>da', ':Lexplore<CR>')
 
+-- yank direct to system clipboard. 
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
+
 
